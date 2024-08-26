39037c67f69842621f884f2f8157495977682f57
Karan-joshi7 <joshikaran94105@gmail.com>
2024-08-26 11:45:50 +0000

    commited file

---
+++ basics/variable.js
@@ -0,0 +1,15 @@
+const accountId=24559334
+let accountEmail="karan@gmail.com"
+var accountPassword="1233456"
+accountCity="khatima"
+
+// accountId=12233444
+
+accountEmail="kanukie@gmail.com"
+accountPassword="33123145"
+accountCity="jaipur"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+// donot ever used variable keyword instead of let

bf94cfe4d550e56aee7da4680a27b0b21da3053c
Karan-joshi7 <joshikaran94105@gmail.com>
2024-08-27 14:19:52 +0000

    data types

---
+++ basics/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as new version
+
+
+//alert(3+3) it will not run as it is node js not browser
+
+
+console.log(3+3)
+
+
+
+let name="Karan"
+let age=18
+let isLoggedin= false
+let state; //it will print undefined as output
+
+
+
+console.log(typeof "karan")
+console.log(typeof undefined)// this will print undefined as output
+console.log(typeof null) //it will print object as an output

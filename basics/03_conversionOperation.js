b8b0a4100967a47c5b45a8d000c254121b26899c
Karan-joshi7 <joshikaran94105@gmail.com>
2024-08-28 19:40:03 +0000

    conversion and operating

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score=33
+console.log( typeof score)
+let Twt= "33 abc "
+console.log(typeof Twt)
+let TwtNumber= Number(Twt)
+console.log(typeof TwtNumber)
+let Kanukie= Boolean(Twt)
+console.log(Kanukie) //when any text is written here otherthan blank in string then it will give as  true 
+
+
+/*let's us start with note some of code we will start with  so let us start with 
+boolean data tyype output in number if 0=>false,1=>true
+in string ""=>false,"anything inside this"=>true 
+*/
+
+
+let Mark=null
+console.log(Mark)
+let numMark=Number(Mark)
+console.log(numMark)
+
+//when someone try to convert the null value in the number then output will zero :)
+
+
+
+
+
+// let us check if we could do this if take us undefined as an input
+let Kiko= undefined
+console.log(typeof Kiko)
+console.log(Kiko)
+let numKiko=Number(Kiko)
+console.log(numKiko) //wheneveer you want to try other data type in other data type it should follow everything to be same data type like here we see that we have undefined as another data type and i try to conveet this whole thing into number so o*p print as nan(not a number)
+console.log(typeof numKiko)

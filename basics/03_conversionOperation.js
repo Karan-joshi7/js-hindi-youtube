f973095bb99bd360c3e0ff4292145aaf012d7cfd
Karan-joshi7 <joshikaran94105@gmail.com>
2024-08-28 21:14:40 +0000

    conversion and operation

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score=33
-console.log( typeof score)
+// console.log( typeof score)
 let Twt= "33 abc "
-console.log(typeof Twt)
+// console.log(typeof Twt)
 let TwtNumber= Number(Twt)
-console.log(typeof TwtNumber)
+// console.log(typeof TwtNumber)
 let Kanukie= Boolean(Twt)
-console.log(Kanukie) //when any text is written here otherthan blank in string then it will give as  true 
+// console.log(Kanukie) //when any text is written here otherthan blank in string then it will give as  true 
 
 
 /*let's us start with note some of code we will start with  so let us start with 
@@ -15,9 +15,9 @@ in string ""=>false,"anything inside this"=>true
 
 
 let Mark=null
-console.log(Mark)
+// console.log(Mark)
 let numMark=Number(Mark)
-console.log(numMark)
+// console.log(numMark)
 
 //when someone try to convert the null value in the number then output will zero :)
 
@@ -27,8 +27,55 @@ console.log(numMark)
 
 // let us check if we could do this if take us undefined as an input
 let Kiko= undefined
-console.log(typeof Kiko)
-console.log(Kiko)
+// console.log(typeof Kiko)
+// console.log(Kiko)
 let numKiko=Number(Kiko)
-console.log(numKiko) //wheneveer you want to try other data type in other data type it should follow everything to be same data type like here we see that we have undefined as another data type and i try to conveet this whole thing into number so o*p print as nan(not a number)
-console.log(typeof numKiko)
+// console.log(numKiko) //wheneveer you want to try other data type in other data type it should follow everything to be same data type like here we see that we have undefined as another data type and i try to conveet this whole thing into number so o*p print as nan(not a number)
+// console.log(typeof numKiko)
+
+
+
+//*******operations********
+
+let value=3
+let negvalue= -value
+// console.log(negvalue)
+// console.log(2+2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2-2)
+// console.log(2**2)
+
+
+let str1="hello"
+let str2="karan"
+let str3= str1+str2
+// console.log(str3)
+
+
+// console.log(2+"2")
+// console.log("2"+2)
+// console.log(2+2+"2")
+// console.log("2"+2+2)
+
+
+// console.log (+true)
+// console.log(true+) this will create error 
+// console.log(+"")
+// console.log(""+) this will create error 
+
+//prefix and postfix
+
+
+// postflix
+let x=3;
+const y=x++
+console.log(`x:${x}, y:${y}`);
+
+
+
+//preflix
+let a=3;
+const b= ++a
+console.log(`a:${a}, b:${b}`);
+

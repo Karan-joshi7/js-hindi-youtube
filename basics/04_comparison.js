1c6930ab0551ec9fbddb022dd6caadd014301b95
Karan-joshi7 <joshikaran94105@gmail.com>
2024-08-29 11:58:36 +0000

    comparison

---
+++ basics/04_comparison.js
@@ -0,0 +1,35 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1); //equal to 
+// console.log(2<1);
+// console.log(1!=2)
+
+
+
+//in some case which are useless ofc we have to cmpare string with number but  it doesn't make any sense at all ,as js it will convert the data type of one and another but don't give according to your desire answer
+
+// console.log("2">1);
+// console.log("02">1);
+//this both will execute eaisly but now always you think it don't convert it into one another
+
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// here we have to do in this way that there are two comparison operator first one <,>,>= and another one is == here in greater or lesser one first change the vslue of null into 0 and then compare the values
+
+
+// console.log(undefined==0);
+// console.log(undefined>=0);
+// console.log(undefined<0);
+
+//undefine ko kisi ke sath bhi compare krle false hi ayega 
+
+
+
+//=== strict check =>this check datatype also
+
+
+
+console.log("2"==2);
+console.log("2"===2);// it will give output as false
